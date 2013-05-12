851bd30b84f046364378d5188a23530f825ac9a6
Juuichi Hirao <hirao@Juuichi-no-Mac-mini.local>
2013-05-12 23:50:21 +0900

    登録画面からの番号入力、回数選択画面の表示方法の修正 　・背景が黒くする部分はアニメーション出来ず・・・

--- RotoSix/UseModalTableViewController.m
+++ RotoSix/UseModalTableViewController.m
@@ -26,23 +26,53 @@
     
     [mainWindow addSubview:modalView];
     
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:0.3f];
-    modalView.center = middleCenter;
-    [UIView commitAnimations];
-    self.view.alpha = 0.8f;
+    UIImageView *imgview = (UIImageView *)[self.view viewWithTag:99];
+
+    if (imgview == nil) {
+        // 黒の空画像を生成してalpha値を変えることによりmodalの背景とする
+        CGRect blackImageFrame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+        CGRect rect = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+        UIGraphicsBeginImageContext(rect.size);
+        CGContextRef contextRef = UIGraphicsGetCurrentContext();
+        CGContextSetFillColorWithColor(contextRef, [UIColor colorWithRed:0 green:0 blue:0 alpha:0.6].CGColor);
+        CGContextFillRect(contextRef, rect);
+        UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        UIImageView *imgview = [[UIImageView alloc] initWithFrame:blackImageFrame];
+        imgview.image = img;
+        imgview.tag = 99;
+        imgview.hidden = NO;
+        [self.view addSubview:imgview];
+        
+        [UIView animateWithDuration:2.0f animations:^{
+            //imgview.alpha = 0.7; // 正常に動作しないので後回し
+            modalView.center = middleCenter;
+        }];
+    }
+    else {
+        // 黒の空画像が既に生成されていた場合はtagから取得
+        UIImageView *imgview = (UIImageView *)[self.view viewWithTag:99];
+        imgview.alpha = 0.6;
+        imgview.hidden = NO;
+        
+        [UIView animateWithDuration:0.5f animations:^{
+            modalView.center = middleCenter;
+        }];
+    }    
 }
 
 - (void) hideModal:(UIView*) modalView
 {
     CGSize offSize = [UIScreen mainScreen].bounds.size;
     CGPoint offScreenCenter = CGPointMake(offSize.width * 0.5f, offSize.height * 1.5f);
-    [UIView beginAnimations:nil context:(__bridge_retained void *)(modalView)];
-    [UIView setAnimationDuration:0.3f];
-    [UIView setAnimationDelegate:self];
-    modalView.center = offScreenCenter;
-    [UIView commitAnimations];
-    self.view.alpha = 1.0f;
+    
+    UIImageView *imgview = (UIImageView *)[self.view viewWithTag:99];
+    [UIView animateWithDuration:0.5f animations:^{
+        modalView.center = offScreenCenter;
+    }];
+
+    // 黒画像を非表示にする
+    imgview.hidden = YES;
 }
 
 - (void) showModalBuyTimesSelect:(NSArray *)arrLottery {
