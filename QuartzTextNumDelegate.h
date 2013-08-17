4272bf6f6dbc10a9a2359fc23829b9d1102dd805
Juuichi Hirao <hirao@Juuichi-no-Mac-mini.local>
2013-08-17 18:20:04 +0900

    番号入力部分にPaste情報を表示するための領域（CALayer）の暫定対応の一時保管

---
+++ QuartzTextNumDelegate.h
@@ -0,0 +1,24 @@
+//
+//  QuartzTextNumDelegate.h
+//  QuartzTest
+//
+//  Created by Hirao Juuichi on 11/02/15.
+//  Copyright 2011 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuartzTextNumDelegate : NSObject {
+}
+
+@property (nonatomic,retain) NSString *strNum;
+//-(id) initWithView: (UIView *)view;
+ 
+/*
+@interface QuartzTextNumDelegate : NSObject {
+	
+}
+
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)myContext;
+ */
+@end
